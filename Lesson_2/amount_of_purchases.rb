af529fadc34935903a799c69b0b87638e2db4e34
Vicstor <vicstorvich@gmail.com>
2018-09-15 11:55:57 +0300

    Lesson_2 end works misstake

--- Lesson_2/amount_of_purchases.rb
+++ Lesson_2/amount_of_purchases.rb
@@ -1,34 +1,33 @@
 puts "Сумма покупок"
 puts
 
-big_hash = Hash.new
-small_hash = Hash.new
-array_amount = Array.new
-arr_total = Array.new
+goods_hash = {}
+price_quantity_hash = {}
+amount_hash = {}
+arr_total_price = []
 
 loop do
 
 print "Введите цену товара за единицу."
-price = gets.chomp
+price = gets.chomp.to_i
 print "Введите колличество товара."
-quantity = gets.chomp
+quantity = gets.chomp.to_i
 print "Введите название товара или 'стоп' если товаров нет."
 goods = gets.chomp
 
 break if goods == "стоп"
 
-total_price_quantity = price.to_i * quantity.to_i
-arr_total << total_price_quantity
+amount_hash[goods] = { price: price, quantity: quantity }
 
-goods_amount = "Сумма за #{goods} равна #{total_price_quantity}"
-array_amount << goods_amount
+total_price_quantity = amount_hash[goods][:price] * amount_hash[goods][:quantity]
+arr_total_price << total_price_quantity
 
-small_hash[price] = quantity
-small_hash.each { |var| big_hash[goods] = [var].to_h }
 
 end
 
-puts
-puts big_hash
-puts array_amount
-puts "Общая стоимость товаров #{arr_total.sum}"
+puts amount_hash
+
+amount_hash.each { |key, value|
+ puts "Сумма за #{ key } равна #{ amount_hash[key][:price] * amount_hash[key][:quantity] } " }
+
+puts "Общая стоимость товаров #{ arr_total_price.sum }"
--- Lesson_2/search_for_numbers.rb
+++ Lesson_2/search_for_numbers.rb
@@ -10,8 +10,8 @@ day = gets.chomp.to_i
 
 days_in_month = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
 
-if year % 4 == 0
-   days_in_month = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+if ((year%4 == 0 and year%100 != 0) or (year%400 == 0))
+   days_in_month[1] = 29
 end
 
 day_in_year = days_in_month.first(month-1).sum
